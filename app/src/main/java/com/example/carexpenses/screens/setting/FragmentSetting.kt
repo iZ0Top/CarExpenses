88a2d81f6fa74fb9b10706861467093e5e4bf64a
admin3 <eco>
2023-03-27 17:57:01 +0300

    04

--- app/src/main/java/com/example/carexpenses/screens/setting/FragmentSetting.kt
+++ app/src/main/java/com/example/carexpenses/screens/setting/FragmentSetting.kt
@@ -1,4 +1,4 @@
-package com.example.carexpenses.screens
+package com.example.carexpenses.screens.setting
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
