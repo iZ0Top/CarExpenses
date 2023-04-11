d04fdce800e88d3edab71d02d91df530bad4e70d
admin3 <eco>
2023-04-11 17:56:58 +0300

    21

--- app/src/main/java/com/example/carexpenses/adapters/add/AdapterAdd.kt
+++ app/src/main/java/com/example/carexpenses/adapters/add/AdapterAdd.kt
@@ -1,10 +1,37 @@
 package com.example.carexpenses.adapters.add
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.carexpenses.databinding.ItemExpenseBinding
+import com.example.carexpenses.model.Expense
 
-class AdapterAdd: RecyclerView.Adapter<AdapterAdd.itemHolder>(){
+class AdapterAdd: RecyclerView.Adapter<AdapterAdd.ItemHolder>(){
 
-    class itemHolder(val binding: ): RecyclerView.ViewHolder(){
+    private var currentList = emptyList<Expense>()
 
+    fun setList(list: List<Expense>){
+        currentList = list
+        notifyDataSetChanged()
+    }
+
+    class ItemHolder(val binding: ItemExpenseBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemExpenseBinding.inflate(layoutInflater, parent, false )
+        return ItemHolder(binding)
+    }
+
+    override fun getItemCount() = currentList.size
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        val expense = currentList[position]
+
+        holder.binding.itemNameOrType.text = expense.name
+        holder.binding.itemDescription.text = expense.description
+        holder.binding.itemPartNumber.text = expense.part_number
+        holder.binding.itemQuantityOrVolume.text = expense.amount.toString()
+        holder.binding.itemPrice.text = expense.sum.toString()
     }
 }
--- app/src/main/java/com/example/carexpenses/screens/add/AddViewModel.kt
+++ app/src/main/java/com/example/carexpenses/screens/add/AddViewModel.kt
@@ -1,12 +1,31 @@
 package com.example.carexpenses.screens.add
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.carexpenses.model.Expense
+import com.example.carexpenses.utils.TAG
 
 class AddViewModel(application: Application): AndroidViewModel(application) {
 
     private var id_group = 0
 
+    val currentExpenseList = MutableLiveData<List<Expense>>()
 
+    fun setGroupToEdit(list: List<Expense>){
+        currentExpenseList.postValue(list)
+    }
+
+    fun createNewGroup(){
+        currentExpenseList.postValue(emptyList())
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG, "AddViewModel. onCleared")
+    }
 
 }
